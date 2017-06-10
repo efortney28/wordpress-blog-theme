142e97887692882d6fd19705cf48ed9602d985d9
efortney28 <efortney28@gmail.com>
2017-06-10 15:57:49 -0500

    version 1.1-small visual changes

--- index.php
+++ index.php
@@ -12,7 +12,12 @@
             the_post();
 ?>
             <div class="post">
-                <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <div class="title">
+                    <h2 class="post-title text-center"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                </div>
+                <div class="meta-info clear-fix">
+                    <?php the_date('F j Y', 'Posted on: '); ?>
+                </div>
                 <div class="post-content"><?php the_content(); ?></div>
             </div>
     
@@ -33,6 +38,10 @@
                     <li><a target="_blank" href="https://efortney28.github.io/"><img class="social" src="<?php echo get_stylesheet_directory_uri(); ?>/images/website.png"></a></li>
                 </ul>
                 <p id="description">The adventures of a newbie coder! I'm trying to learn as much as possible about all things web development in order to become a full-stack web developer someday soon. Please check out my social media links and website link if you are interested in following me along the way!</p>
+                <?php if(is_user_logged_in()) {
+                        echo "<div class=\"new-post\"><a target=\"_blank\" href=\"/wp-admin/post-new.php\"><button type=\"button\" class=\"btn btn-primary\">New Post</button></a></div>";
+                }
+                ?>
             </aside>
         </div>
     </div>
